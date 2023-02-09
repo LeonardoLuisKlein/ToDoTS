eb5bf18aca6bb96031b91ffa8c533fe6912e73ad
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2023-02-09 15:29:37 -0300

    Feat: creating types for every task

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import * as C from "./App.styles"
+import { Item } from './types/Item'
+
 
 const App = () => {
+
+  const [list, setList] = useState<Item []>([
+    { id: 1, name: 'Comprar o pão na padaria', done: false},
+    {id: 2, name: 'Ir na academia', done: true}
+  ])
+
   return (
     <C.Container>
-      ...
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+        {list.map((item, index) =>(
+          <div>{item.name}</div>
+        ))}
+      </C.Area>
     </C.Container>
   );
 }
